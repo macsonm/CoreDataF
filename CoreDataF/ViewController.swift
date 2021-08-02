c78d679cb5debfac15962ce06959dd8f587e4003
macsonm <macson5.m@yandex.ru>
2021-08-02 06:30:33 -0700

    Initial Commit

---
+++ CoreDataF/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataF
+//
+//  Created by mac on 02.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
